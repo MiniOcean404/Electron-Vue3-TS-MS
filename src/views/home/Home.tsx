1649319240fb781577365fc96dff682b88117469
戴海洋 <1037306928@qq.com>
2021-08-22 01:27:13 +0800

    检验账号cookie有效性

--- src/views/home/Home.tsx
+++ src/views/home/Home.tsx
@@ -3,14 +3,29 @@ import { useStore } from 'vuex'
 import Login from 'views/home/login/Login.tsx'
 import Table from 'views/home/user-table/Table.tsx'
 import { checkUser } from 'api/user'
+import { cycleUser } from 'common/utils'
+import { notification } from 'ant-design-vue'
 
 export default defineComponent({
 	name: 'Home',
 	setup(props, context) {
 		const store = useStore()
-		const Cookie = ref('')
 		const allUser = computed(() => store.getters['user/userInfo'])
+		;(function checkAllUser() {
+			if (allUser.value.length >= 0) {
+				cycleUser(allUser.value, checkCookieValid)
+			}
+		})()
 
+		function checkCookieValid(u: { name: string; cookie: string }) {
+			checkUser(u.cookie).then((res: any) => {
+				if (res.data !== 'false') {
+					notification['error']({ message: `用户${u.name}的cookie已经失效，请重新登录` })
+				}
+			})
+		}
+
+		const Cookie = ref('')
 		function alreadyLogin(cookie: string) {
 			Cookie.value = cookie
 			const pinId = cookie.match(/pinId=(.*?);/)?.[1]
